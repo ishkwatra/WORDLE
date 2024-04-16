add52fd7dc232b7379918dcb8f56ca857e3da3b9
Ish Kwatra <77015916+ishkwatra@users.noreply.github.com>
2024-04-16 18:03:43 +0530

    Update wordle.py
    made input case insensitive

--- wordle.py
+++ wordle.py
@@ -6,40 +6,50 @@ print("WELCOME TO WORDLE!")
 input("PRESS ENTER TO CONTINUE...")
 
 print("My first guess is - R H Y M E")
+w1 = "RHYME"
 g1 = input("Enter the result as a string of W, Y, G (White, Yellow, Green) - ")
+g1 = g1.upper()
 if g1 == "GGGGG":
-    print("\nYour word is -", "RHYME")
+    print("\nYour word is -", w1)
     exit(0)
 
 print("My second guess is - P L A N T")
+w2 = "PLANT"
 g2 = input("Enter the result as a string of W, Y, G (White, Yellow, Green) - ")
+g2 = g2.upper()
 if g2 == "GGGGG":
-    print("\nYour word is -", "PLANT")
+    print("\nYour word is -", w2)
     exit(0)
 
 print("My third guess is - S Q U I Z")
+w3 = "SQUIZ"
 g3 = input("Enter the result as a string of W, Y, G (White, Yellow, Green) - ")
+g3 = g3.upper()
 if g3 == "GGGGG":
-    print("\nYour word is -", "SQUIZ")
+    print("\nYour word is -", w3)
     exit(0)
 
 print("My fourth guess is - F J O R D")
+w4 = "FJORD"
 g4 = input("Enter the result as a string of W, Y, G (White, Yellow, Green) - ")
+g4 = g4.upper()
 if g4 == "GGGGG":
-    print("\nYour word is -", "FJORD")
+    print("\nYour word is -", w4)
     exit(0)
 
 print("My fifth guess is - B L O C K")
+w5 = "BLOCK"
 g5 = input("Enter the result as a string of W, Y, G (White, Yellow, Green) - ")
+g5 = g5.upper()
 if g5 == "GGGGG":
-    print("\nYour word is -", "BLOCK")
+    print("\nYour word is -", w5)
     exit(0)
 
 ans = ["_"]*5
 pool = []
 alp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-temp = "RHYMEPLANTSQUIZFJORDBLOCK"
+temp = w1+w2+w3+w4+w5
 guess = g1+g2+g3+g4+g5
 
 for i in range(25):
